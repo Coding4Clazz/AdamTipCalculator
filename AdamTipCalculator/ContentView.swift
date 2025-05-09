ad3a0c38a85b43019a8482910c18d83512afe530
Adam Reid <reidad@merrimack.edu>
2025-05-08 22:46:03 -0400

    adding tweaks to improve the way app looks (font, display colors, slider view)

--- AdamTipCalculator/ContentView.swift
+++ AdamTipCalculator/ContentView.swift
@@ -24,18 +24,24 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Bill Amount")) {
+                Section(header: Text("Bill Amount").fontWeight(.bold)) {
                     TextField("Enter Tab...", text: $billTotal)
                         .keyboardType(.decimalPad)
                 }
-                Section(header:  Text("Preferred Tip Percent")) {
-                    Slider(value: $tipPercent, in: 10...25, step: 2.5)
-                    Text("Tip Percent: \(tipPercent)%")
+                Section(header:  Text("Preferred Tip Percent").fontWeight(.bold)) {
+                    HStack{
+                        Slider(value: $tipPercent, in: 10...25, step: 2.5)
+                        Text("Percent: \(Int(tipPercent))%")
+                            .font(.subheadline)
+                    }       .foregroundColor(.purple)
                 }
                 //Calc results
                 Section(header: Text("Calculations")) {
                     Text("Tip: $\(tipTotal, specifier: "%.2f")")
+                        .foregroundColor(.cyan)
                     Text("Total: $\(totalBill, specifier: "%.2f")")
+                        .foregroundColor(.blue)
+                        .fontWeight(.semibold)
                 }
             }
             .navigationBarTitle("Adam's Tip Calculator")
