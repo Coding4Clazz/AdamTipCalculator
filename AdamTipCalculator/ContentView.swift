67338e86c85891afad969afc8c4d633838275531
Adam Reid <reidad@merrimack.edu>
2025-05-08 22:15:21 -0400

    adding form layout

--- AdamTipCalculator/ContentView.swift
+++ AdamTipCalculator/ContentView.swift
@@ -8,17 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var billTotal = ""
+    @State private var tipPercent = 20.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text("Bill Amount")) {
+                    TextField("Enter Tab...", text: $billTotal)
+                        .keyboardType(.decimalPad)
+                }
+                Section(header:  Text("Preferred Tip Percent")) {
+                    Slider(value: $tipPercent, in: 10...25, step: 2.5)
+                    Text("Tip Percent: \(tipPercent)%")
+                }
+            }
+            .navigationBarTitle("Adam's Tip Calculator")
         }
-        .padding()
     }
 }
+        
 
 #Preview {
     ContentView()
 }
+
